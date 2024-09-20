42751ef8545a82e69b433c2c79d012851efa3d32
hamza <hamzashakil181@gmail.com>
2024-09-20 11:38:54 -0700

    work accomplish

---
+++ assignment.js
@@ -0,0 +1,166 @@
+// assignment 15
+// Chapter 10
+// Q1
+// var city = prompt("Enter City Name")
+// if(city === "Karachi"){
+//     console.log("The City Of lights")
+// }
+// Q2
+// var x = "y"
+// if(x === "y"){
+//     var z = prompt("What Is The Value Of Z")
+// }
+// Q3
+// var zipCode = +prompt("Enter A Zip Code")
+// if(zipCode===10010){
+//     alert("Karachi")
+// }
+// else{
+//     alert("Please Write Correct City")
+// }
+// Q4
+// var x = 7
+// if(x === 7){
+//     x = 1
+// }
+// console.log(x)
+// Chapter 11
+// Q1
+// var x = u
+// if(x !== u ){}
+// Q2
+// var num = 53
+// var num_2 = 90
+// if(num>=num_2){}
+// Q3
+// var y = 8
+// if(y !== 6){
+    // y=6
+// }
+// console.log(y)
+// Q4
+// var num = 40
+// if(num !== 50){
+    // alert("Congratulations")
+// }
+// Q5
+// var name = prompt("Enter Your Name")
+// if(name !== "Abdul Ahad"){
+//     alert("No Match")
+// }
+// Chapter 12
+// // Q1
+// var num = +prompt("Enter A First Num")
+// var num_2 = +prompt("Enter A Second Num")
+// if(num >= num_2){
+//     alert("Number 1 is greater than num 2")
+// }
+// else{
+//     alert("Number 1 is less than num 2")
+// }
+// Q2
+// var total = +prompt("Enter Your Total Marks")
+// var obtain = +prompt("Enter your Obtain Marks")
+// var percentage = obtain/total * 100
+// var grade 
+// if(percentage <= 90){
+    // grade = "A+"
+// }
+// if(percentage <= 80){
+//     grade = "A"
+// }
+// if(percentage <= 70){
+//     grade = "B"
+// }
+// if(percentage <= 60){
+//     grade = "C"
+// }
+// if(percentage <= 50){
+//     grade = "D"
+// }
+// if(percentage <= 50){
+//     grade = "Fail"
+// }
+// alert (`your percentage is ${percentage} and grade is ${grade}`)
+// Q3
+// var a = 10
+// if (a === 10) {
+//     alert("a is 10");
+// } else {
+//     alert("The correct value of a is " + a);
+// }
+// Q4
+// var city = prompt("Enter Your City Name")
+// if(city === "Karachi"){
+//     alert("acknowledging it is Karachi")
+// }
+// if(city==="Lahore"){
+//     alert("acknowledging it is Lahore")
+//     }
+// else{
+//     alert("Please Enter Correct City")
+// }
+// Chapter 13
+// Q1
+// var a
+// var b
+// var c
+// var d
+// if(a===b && c===d){}
+// Q2
+// if(a===b || c!==d){}
+// Q3
+// var name = prompt("Ennter Your Name")
+// var age = +prompt("Your Age")
+// if(name === "Abdul Ahad" || name === "Zee" || age <=60){
+    // console.log("True")
+// }
+// Q4
+// var num = 30
+// var num_2 = 70
+// if( num > num_2){
+//     alert("Num 1 is less than 2nd num")
+// }
+// else{
+//     alert("Num 1 is greater than 2nd num")
+// }
+// // Q5
+// var firstName = "Abdul"
+// var lastName = "Ahad"
+// var ask = prompt("Enter your first name")
+// var ask_2 = prompt("Enter your last name")
+// if(ask === firstName && ask_2===lastName){
+//     alert("Your Name Matched")
+// }
+// else{
+//     alert("Please Write The Correct Name")
+// }
+// Chapter 14
+// Q1
+// var password = +prompt("Enter Password")
+// if(password <=5){
+    // alert("Password Must Be greater than 5")
+// }
+// else{
+    // alert("Ok")
+// }
+// Q2
+// var a = 1
+// var c = "Max"
+// if (a === 1) {
+    // if (c === "Max") {
+        // alert("OK");
+    // }
+// }
+// Q3
+// if(a === 1 && c === "Max"){
+    // alert("Ok")
+// }
+// Q4
+// var a = 10
+// var b = 10
+// if(a===b){
+//     if(a <= b){
+//         alert("True")
+//     }
+// }
---
+++ practice.js
@@ -0,0 +1,85 @@
+// Practice File
+// Q1
+// var char = prompt("Enter A Character")
+// if(char >= "0" && char <= "9"){
+//     alert("It's a number")
+// }
+// else if(char >= "A" && char <= "Z"){
+//     alert("It's a upper case")
+// }
+// else if(char >= "a" && char <= "z"){
+//     alert("It's a lower case")
+// }
+// else{
+//     alert("Invalid")
+// }
+// Q2
+// var num = +prompt("Enter A First Num")
+// var num_2 = +prompt("Enter A Second Num")
+// if(num > num_2){
+//     alert("The Larger Num Is " + num)
+// }else if(num < num_2){
+//     alert(`The Larger Num Is ${num_2}`)
+// }
+// else{
+//     alert("Both Are Equal Num")
+// }
+// Q3
+// var num = +prompt("Enter A Num")
+// if(num > 0){
+//     alert("The Num Is Positive")
+// }
+// else if(num < 0){
+//     alert("The Num Is Negative")
+// }
+// else{
+//     alert("The Num Is Zero")
+// }
+// Q4
+// var char = prompt("Write A Vowel")
+// if(char ==="a" || char === "e" || char === "i"|| char === "o"|| char === "u" || char === "A"|| char === "E"|| char === "I"|| char === "O"|| char === "U"){
+//     alert("true")
+// }
+// else{
+//     alert("False")
+// }
+// Q5
+// var pass = prompt("Enter A Password")
+// var pass_2 = prompt("Enter A Confrim Password")
+// if(pass === "" || pass_2 === ""){
+//     alert("Please Enter Your Password")
+// }
+// else if (pass === pass_2){
+//     alert("Correct password")
+// }
+// else{
+//     alert("Incorrect Password")
+// }
+// Q6
+// var greeting;
+// var hour = +prompt("Enter A Time")
+// if (hour < 18) {
+//     alert(greeting = "Good day")
+    
+// }
+// else{
+
+// alert( greeting = "Good evening")
+// }
+// // Q7
+// var time = +prompt("Enter A Time")
+// if(time >= 0 && time < 1200){
+//     alert("Good Morning")
+// }
+// else if(time >= 1200 && time < 1700){
+//         alert("Good Afternoon")
+// }
+// else if(time >= 1700 && time < 2100){
+//     alert("Good Evening")
+// }
+// else if(time >= 2100 && time <= 2359){
+//     alert("Good Night")
+// }
+// else{
+//     alert("Please Enter Valid Time")
+// }
